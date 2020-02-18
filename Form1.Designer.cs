4f63f5f84cf3bf4141e3ad20f34be38241d5a0d3
sosman10 <sosman10@myseneca.ca>
2020-02-17 20:44:33 -0500

    2020-02-17 08:42 PM Selim Osman [Main Form: Added form elements(textboxes) for entering username/password. Also added form elements(buttons) for connecting to the server, signing up for a new account, and exiting the application. ]

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.userTxtBox = new System.Windows.Forms.TextBox();
+            this.signUpBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.passTxtBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.connectBtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(258, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Display Name:";
+            // 
+            // userTxtBox
+            // 
+            this.userTxtBox.Location = new System.Drawing.Point(160, 153);
+            this.userTxtBox.Name = "userTxtBox";
+            this.userTxtBox.Size = new System.Drawing.Size(303, 22);
+            this.userTxtBox.TabIndex = 1;
+            // 
+            // signUpBtn
+            // 
+            this.signUpBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.signUpBtn.Image = global::Chat_Messenger_Client.Properties.Resources.signUpBtn;
+            this.signUpBtn.Location = new System.Drawing.Point(200, 340);
+            this.signUpBtn.Name = "signUpBtn";
+            this.signUpBtn.Size = new System.Drawing.Size(231, 43);
+            this.signUpBtn.TabIndex = 2;
+            this.signUpBtn.UseVisualStyleBackColor = true;
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitBtn.Image = global::Chat_Messenger_Client.Properties.Resources.exitBtn;
+            this.exitBtn.Location = new System.Drawing.Point(200, 389);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(231, 43);
+            this.exitBtn.TabIndex = 3;
+            this.exitBtn.UseVisualStyleBackColor = true;
+            // 
+            // passTxtBox
+            // 
+            this.passTxtBox.Location = new System.Drawing.Point(160, 222);
+            this.passTxtBox.Name = "passTxtBox";
+            this.passTxtBox.Size = new System.Drawing.Size(303, 22);
+            this.passTxtBox.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(273, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password:";
+            // 
+            // connectBtn
+            // 
+            this.connectBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.connectBtn.Image = global::Chat_Messenger_Client.Properties.Resources.connectBtn;
+            this.connectBtn.Location = new System.Drawing.Point(200, 291);
+            this.connectBtn.Name = "connectBtn";
+            this.connectBtn.Size = new System.Drawing.Size(231, 43);
+            this.connectBtn.TabIndex = 6;
+            this.connectBtn.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(73, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(503, 55);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Chat Messenger Plus";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::Chat_Messenger_Client.Properties.Resources.back_plate;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(658, 463);
+            this.ControlBox = false;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.connectBtn);
+            this.Controls.Add(this.passTxtBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.signUpBtn);
+            this.Controls.Add(this.userTxtBox);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox userTxtBox;
+        private System.Windows.Forms.Button signUpBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.TextBox passTxtBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button connectBtn;
+        private System.Windows.Forms.Label label3;
     }
 }
 
